32bc107bf31542dc0d6d3475decdad6df250f163
MITESHDABHI <dabhimitesh18@gmail.com>
2023-03-06 11:09:35 +0000

    first commit

---
+++ core/tests/test_ex1.py
@@ -0,0 +1,20 @@
+# import pytest
+
+
+# @pytest.fixture(scope="session")
+# def fixture1():
+#     print("Run fixture 1")
+#     return 1
+
+
+# def test_example1(fixture1):
+#     print("Run example 1")
+#     num = fixture1
+#     assert num == 1
+
+# def test_example2(fixture1):
+#     print("Run example 2")
+#     num = fixture1
+#     assert num == 1
+
+
