cc8e148025e0159c91793135a5b1bf1023bc90e2
MITESHDABHI <dabhimitesh18@gmail.com>
2023-03-07 13:15:49 +0000

    added changes

---
+++ core/tests/factories.py
@@ -0,0 +1,12 @@
+# import factory
+# from faker import Faker
+# from django.contrib.auth.models import User
+# faker = Faker()
+# class Userfactory(factory.django.DjangoModelFactory):
+#     class Meta:
+#         model = User
+    
+#     username = faker.name()
+#     is_staff = "True"
+    
+    
